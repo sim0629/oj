b47e59710f17b1a3d82e45cf458346ed89901bfb
Gyumin Sim <gyumin.sim@privatenote.co.kr>
2022-12-21 03:22:07 +0900

    Modify algospot/nerd2.cpp
    set to map

--- algospot/nerd2.cpp
+++ algospot/nerd2.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <iostream>
 #include <iterator>
+#include <map>
 #include <set>
 #include <stack>
 #include <string>
@@ -14,48 +15,32 @@
 using namespace std;
 
 int N;
-int p[50000];
-int q[50000];
+map<int, int> pareto;
 
-void print_set(const set<pair<int, int>>& s) {
-  for (auto pq : s) {
-    cout << '(' << pq.first << ',' << pq.second << ')' << ' ';
+void insertToPareto(int p, int q) {
+  auto it_bigger = pareto.lower_bound(p);
+  if (it_bigger != pareto.end()) {
+    if ((*it_bigger).second > q) {
+      return;
+    }
   }
-  cout << '\n';
-}
-
-int solve() {
-  int total = 0;
-  set<pair<int, int>> pareto;
-  for (int i = 0; i < N; i++) {
-    auto pq = make_pair(p[i], q[i]);
-    auto it_bigger = lower_bound(pareto.begin(), pareto.end(), pq);
-    if (it_bigger != pareto.end()) {
-      if ((*it_bigger).second > q[i]) {
-        total += pareto.size();
-        continue;
+  auto it = pareto.insert(make_pair(p, q)).first;
+  if (it != pareto.begin()) {
+    auto next_it = prev(it);
+    while (true) {
+      it = next_it;
+      if ((*it).second > q) {
+        break;
+      }
+      if (it == pareto.begin()) {
+        pareto.erase(it);
+        break;
+      } else {
+        next_it = prev(it);
+        pareto.erase(it);
       }
     }
-    auto it = pareto.insert(pq).first;
-    if (it != pareto.begin()) {
-      auto next_it = prev(it);
-      while (true) {
-        it = next_it;
-        if ((*it).second > q[i]) {
-          break;
-        }
-        if (it == pareto.begin()) {
-          pareto.erase(it);
-          break;
-        } else {
-          next_it = prev(it);
-          pareto.erase(it);
-        }
-      }
-    }
-    total += pareto.size();
   }
-  return total;
 }
 
 int main(void) {
@@ -63,11 +48,16 @@ int main(void) {
   int C;
   cin >> C;
   while (C--) {
+    int p, q;
     cin >> N;
+    pareto.clear();
+    int total = 0;
     for (int i = 0; i < N; i++) {
-      cin >> p[i] >> q[i];
+      cin >> p >> q;
+      insertToPareto(p, q);
+      total += pareto.size();
     }
-    cout << solve() << '\n';
+    cout << total << '\n';
   }
   return 0;
 }
